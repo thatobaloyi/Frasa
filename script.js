ae37897789bb5ee39543ad365ebcf0ea3a9dde19
Thato Baloyi <thato2313321@gmail.com>
2023-08-23 06:19:59 +0200

    navbar update

--- script.js
+++ script.js
@@ -6,18 +6,34 @@ const menu = document.querySelector(".nav_menu")
 const menuOpen = document.querySelector(".menu_open")
 const menuClose = document.querySelector(".menu_close")
 
+window.addEventListener("resize", myFunction);
+function myFunction(){
+    if(window.innerWidth < 1024){
+        menuOpen.style.display = 'inline-block'
+        menuClose.style.display = 'none'
+        menu.style.display = 'none'
+    }
+
+    if(window.innerWidth > 1024){
+        menuOpen.style.display = 'none'
+        menuClose.style.display = 'none'
+        menu.style.display = 'flex'
+    }
+}
+
 menuOpen.addEventListener('click', () => {
     menu.style.display = 'flex'
+    menuClose.style.display = 'inline-block'
     menuOpen.style.display = 'none'
-    menuClose.style.display = 'block'
-    menuClose.style.backgroundColor = 'transparent'
-    
 })
 
-menuClose.addEventListener('click', () => {
+const closeMenu = () =>{
     menu.style.display = 'none'
-    menuOpen.style.display = 'block'
     menuClose.style.display = 'none'
-})
+    menuOpen.style.display = 'inline-block'
+}
+
+menuClose.addEventListener('click', closeMenu)
+
 
 
